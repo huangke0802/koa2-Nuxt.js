36469e1e5bc0993cd3cccbada3f00266cd82cb97
huangke <huangke0802@163.com>
2019-01-27 12:56:38 +0800

    仿照美团做的一个项目

---
+++ mt-app/components/public/header/searchbar.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="search-panel">
+    <el-row class="m-header-searchbar">
+      <el-col :span="3" class="left">
+        <img src="//s0.meituan.net/bs/fe-web-meituan/e5eeaef/img/logo.png" alt>
+      </el-col>
+      <el-col :span="15" class="center">
+							<div class="wrapper">
+								<el-input placeholder="搜索商家或地点"></el-input>
+								<button class="el-button el-button-primary">
+									<i class="el-icon-search" />
+								</button>
+							</div>
+							<p class="suggset">
+								<a href="#">故宫博物院</a>
+								<a href="#">故宫博物院</a>
+								<a href="#">故宫博物院</a>
+								<a href="#">故宫博物院</a>
+								<a href="#">故宫博物院</a>
+							</p>
+						</el-col>
+      <el-col class="right" :span="6">
+        <ul class="security">
+          <li>
+            <i class="refund"></i>
+            <p class="txt">随时退</p>
+          </li>
+          <li>
+            <i class="single"></i>
+            <p class="txt">不满意免单</p>
+          </li>
+          <li>
+            <i class="overdue"></i>
+            <p class="txt">过期退</p>
+          </li>
+        </ul>
+      </el-col>
+    </el-row>
+  </div>
+</template>
+<script>
+export default {
+
+}
+</script>
+<style scoped>
+</style>
---
+++ mt-app/layouts/default.vue
@@ -0,0 +1,22 @@
+<template>
+  <el-container class="layout-default">
+    <el-header>
+      <my-header/>
+    </el-header>
+    <el-main>
+      <nuxt/>
+    </el-main>
+    <el-footer></el-footer>
+  </el-container>
+</template>
+
+<script>
+import MyHeader from "@/components/public/header/index.vue"
+export default {
+  components: {
+    MyHeader
+  }
+}
+</script>
+<style>
+</style>
